d831d0f8d04282307c686c475d98d1ac0cab92fb
Luckyhudda <mr.luckyhudda@gmail.com>
2023-10-21 16:41:07 +0530

    main components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Table from './components/Table/Table'
 function App() {
  
   return (
     <>
-    
+    <Header/>
+    <Table/>
     </>
   )
 }
