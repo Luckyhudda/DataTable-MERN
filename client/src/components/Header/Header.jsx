2daef27095dc26dbb1caa1c91fb8ef9eb3f43e12
Luckyhudda <mr.luckyhudda@gmail.com>
2023-11-09 20:36:37 +0530

    change

--- client/src/components/Header/Header.jsx
+++
@@ -1,6 +0,0 @@
-
-const Header = () => {
-  return <div className="h-[65px] bg-sky-700 text-white font-sans text-[26px] flex justify-center items-center font-semibold">Advanced interaction controls to Tables</div>;
-}
-
-export default Header;
